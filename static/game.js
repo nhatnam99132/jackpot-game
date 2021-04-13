91445994672b76638f2988794bd65810e5328422
bibu <hoanganh51199@gmail.com>
2021-04-13 10:47:13 +0700

    them event di chuyen file game.js

--- static/game.js
+++ static/game.js
@@ -10,3 +10,26 @@ function gameStart(){
     $("#start-screen").hide();
 }
 $("#start-button").on('click', gameStart);
+
+let movement = {};
+
+$(document).on('keydown keyup', (event) => {
+    const KeyToCommand = {
+        'ArrowUp': 'forward',
+        'ArrowDown': 'back',
+        'ArrowLeft': 'left',
+        'ArrowRight': 'right',
+    };
+    const command = KeyToCommand[event.key];
+    if(command){
+        if(event.type === 'keydown'){
+            movement[command] = true;
+        }else{ /* keyup */
+            movement[command] = false;
+        }
+        socket.emit('movement', movement);
+    }
+    if(event.key === ' ' && event.type === 'keydown'){
+        socket.emit('shoot');
+    }
+});
