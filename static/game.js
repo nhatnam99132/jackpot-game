c4e5c2e310991fbb05014680bcf295381078516b
bibu <hoanganh51199@gmail.com>
2021-04-13 10:52:42 +0700

    Tao su kien cho player,thanh mau, diem, hinh anh

--- static/game.js
+++ static/game.js
@@ -33,3 +33,44 @@ $(document).on('keydown keyup', (event) => {
         socket.emit('shoot');
     }
 });
+socket.on('state', function(players, bullets, walls) {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    context.lineWidth = 10;
+    context.beginPath();
+    context.rect(0, 0, canvas.width, canvas.height);
+    context.stroke();
+
+    Object.values(players).forEach((player) => {
+        context.save();
+        context.font = '20px Bold Arial';
+        context.fillText(player.nickname, player.x, player.y + player.height + 25);
+        context.font = '10px Bold Arial';
+        context.fillStyle = "gray";
+        context.fillText('♥'.repeat(player.maxHealth), player.x, player.y + player.height + 10);
+        context.fillStyle = "red";
+        context.fillText('♥'.repeat(player.health), player.x, player.y + player.height + 10);
+        context.translate(player.x + player.width/2, player.y + player.height/2);
+        context.rotate(player.angle);
+        context.drawImage(playerImage, 0, 0, playerImage.width, playerImage.height, -player.width/2, -player.height/2, player.width, player.height);
+        context.restore();
+        
+        if(player.socketId === socket.id){
+            context.save();
+            context.font = '30px Bold Arial';
+            context.fillText('You', player.x, player.y - 20);
+            context.fillText(player.point + ' point', 20, 40);
+            context.restore();
+        }
+    });
+    Object.values(bullets).forEach((bullet) => {
+        context.beginPath();
+        context.arc(bullet.x, bullet.y, bullet.width/2, 0, 2 * Math.PI);
+        context.stroke();
+    });
+    Object.values(walls).forEach((wall) => {
+        context.fillStyle = 'black';
+        context.fillRect(wall.x, wall.y, wall.width, wall.height);
+    });
+    
+});
